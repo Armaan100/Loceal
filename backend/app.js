e6b728801872a6bcef0a764b272b134a1ca608a5
Armaan Gogoi <agogoi_be23@thapar.edu>
2025-10-04 19:24:24 +0530

    backend server set up

--- backend/app.js
+++ backend/app.js
@@ -3,3 +3,17 @@ dotenv.config();
 
 const express = require("express");
 const app = express();
+
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+
+const cors = require("cors");
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(logger("dev"));
+app.use(cookieParser());
+
+
+module.exports = app;
