8070fb71cf5ecef8a57eeba1730abcea2910aef7
Armaan Gogoi <agogoi_be23@thapar.edu>
2025-10-12 20:02:33 +0530

    Intital Admin  Model Desgined

---
+++ backend/models/admin.model.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const AdminSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+AdminSchema.index({email: 1});
+
+AdminSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id}, process.env.JWT_SECRET, {expiresIn: "1d"});
+    return token;
+}
+
+AdminSchema.methods.hashPassword = async function(password){
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+}
+
+AdminSchema.methods.comparePassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+
+module.exports = mongoose.model("Seller", AdminSchema);
