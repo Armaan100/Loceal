6fa7158a93ffaf76aa536944286d0e5a088a650e
Armaan Gogoi <agogoi_be23@thapar.edu>
2025-10-23 20:35:07 +0530

    Added auth customer

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,42 @@
+const CustomerModel = require("../models/customer.model");
+const SellerModel = require("../models/seller.model");
+// const AdminModel = require(""../models/admin.model")
+
+const jwt = require("jsonwebtoken");
+
+module.exports.authCustomer = async (req, res, next) => {
+    try{
+        const token = req.cookies.token || req.headers.authorization.split(' ')[1]; // milam frontend r lgt jetia bonam
+        
+        if(!token){
+            return res.status(401).json({
+                message: "Unauthorized"
+            });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if(!decoded || !decoded._id){
+            return res.status(401).json({
+                message: "Unauthorized. Invalid token."
+            });
+        }
+
+        const customer = await CustomerModel.findById(decoded._id);
+
+        if(!customer){
+            return res.status(404).json({
+                message: "Customer not found."
+            });
+        }
+
+        req.customer = customer;
+        next();
+    }catch(err){
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
+
