4c0b12bb0c0667fc37fa2b13e0e12e42e54df62b
Armaan Gogoi <agogoi_be23@thapar.edu>
2025-10-23 20:38:53 +0530

    Added Auth Admin

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 const CustomerModel = require("../models/customer.model");
 const SellerModel = require("../models/seller.model");
-// const AdminModel = require(""../models/admin.model")
+const AdminModel = require("../models/admin.model")
 
 const jwt = require("jsonwebtoken");
 
@@ -68,6 +68,43 @@ module.exports.authSeller = async (req, res, next) => {
         }
 
         req.seller = seller;
+        next();
+    }catch(err){
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
+
+
+module.exports.authAdmin = async (req, res, next) => {
+    try{
+        const token = req.cookies.token || req.headers.authorization.split(' ')[1]; // milam frontend r lgt jetia bonam
+        
+        if(!token){
+            return res.status(401).json({
+                message: "Unauthorized"
+            });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if(!decoded || !decoded._id){
+            return res.status(401).json({
+                message: "Unauthorized. Invalid token."
+            });
+        }
+
+        const admin = await AdminModel.findById(decoded._id);
+
+        if(!admin){
+            return res.status(404).json({
+                message: "Admin not found."
+            });
+        }
+
+        req.admin = admin;
         next();
     }catch(err){
         return res.status(500).json({
