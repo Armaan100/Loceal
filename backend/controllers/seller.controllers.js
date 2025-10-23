f7addc839f7aa1658cd65480354e27bbb5234ba1
Armaan Gogoi <agogoi_be23@thapar.edu>
2025-10-23 20:10:32 +0530

    Added Login Seller

--- backend/controllers/seller.controllers.js
+++ backend/controllers/seller.controllers.js
@@ -193,7 +193,32 @@ module.exports.VerifyCustomer = async (req, res) => {
 }
 
 module.exports.Login = async (req, res) => {
-
+    const {email, password} = req.body;
+    
+        const seller = await SellerModel.findOne({email: email}).select("+password");
+    
+        if(!seller){
+            return res.status(400).json({
+                message: "Seller not found"
+            })
+        }
+    
+        const validPassword = await seller.comparePassword(password);
+    
+        if(!validPassword){
+            return res.status(400).json({
+                message: "Invalid Credentials"
+            })
+        }
+    
+        const token = seller.generateAuthToken();
+        res.cookie("token", token);
+    
+        res.status(200).json({
+            message: "Seller logged in successfully",
+            seller,
+            token
+        });
 }
 
 module.exports.Logout = async (req, res) => {
